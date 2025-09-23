51d25dfd8d644660906d5fbe6ae946baf2c36b74
Bencevrga <2005bencevarga@gmail.com>
2025-09-24 00:41:29 +0200

    fv

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  outDir: 'docs',
-  // GitHub Pages alatt a repo-útvonal miatt érdemes:
-  base: '/laaurainterior',
-  site: 'https://github.com/Bencevrga/laaurainterior',
+  scopedStyleStrategy: 'where', // biztonságosabb CSS kapszulázás
 });
